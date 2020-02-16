1fdafa084a3f19914dc2349180f97ed2b07d9c75
Raj D <rajd@Rajs-MacBook-Air.local>
2020-02-17 00:39:07 +0530

    yup

--- classes.py
+++ classes.py
@@ -4,6 +4,7 @@
 
 class Point:
     # constructor
+    # instance method
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -13,13 +14,44 @@ class Point:
 
     default_color = "blue"
 
-    # methods
+    # instance methods
     def draw(self):
         print(f"Point: ({self.x}, {self.y})")
 
+    # class method
+    # is a factory method
     @classmethod
     def zero(cls):
+        # instead of point = Point(0,0)  we will write .. point = Point.zero()
         return cls(0, 0)
+        # refering to same class with predefined values.. and returning the new object
+
+    # magic methods
+    # comparision magic method
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return self.x != other.x and self.y != other.y
+
+    def __lt__(self, other):
+        return self.x < other.x and self.y < other.y
+
+    def __gt__(self, other):
+        return self.x > other.x and self.y > other.y
+
+    def __le__(self, other):
+        return self.x <= other.x and self.y <= other.y
+
+    def __ge__(self, other):
+        return self.x >= other.x and self.y >= other.y
+
+    # numeric magic method
+    def __add__(self, other):
+        return (self.x + other.x, self.y + other.y)
 
 
 # changing class level attributes
@@ -27,6 +59,19 @@ Point.default_color = "lightBlue"
 
 # self refer to the current object and its properties
 point = Point(2, 3)
-point2 = Point.zero()
+
+# class methods
+point2 = Point.zero()  # is a factory method
+# instance methods
 point2.draw()
 point.draw()
+
+# magic methods
+print(str(point))
+print(point == point2)
+print(point != point2)
+print(point >= point2)
+print(point <= point2)
+print(point < point2)
+print(point > point2)
+print(point + point2)
