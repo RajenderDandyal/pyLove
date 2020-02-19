d6a25f41388a2944ca20fbbd3c6e990efde4ee5d
Raj D <rajd@Rajs-MacBook-Air.local>
2020-02-19 17:12:17 +0530

    yup

--- topicsLeft.py
+++ topicsLeft.py
@@ -1,1 +1,22 @@
 # generators in py
+
+
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# $$$$$$$$$$$$$$$$$$$$$$ comands $$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+# generate docs of libraries
+# pydoc3 -w <lib name>
+# ex:-
+# pydoc3 -w math
+# open math.html
+# generates .html file .. with docs of that module .. to be opened in browser
+
+
+# pipenv to activate the current venv
+# pipenv shell
+# exit
+
+# install new packages
+# pipenv install  .... same as ... npm install
+# pip install request .... same as .... npm install request
